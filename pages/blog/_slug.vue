80ce7951a543e1f4a9e63530e516c8b45c1a8401
Jefth Valentim <jefthvalentim@hotmail.com>
2021-01-22 09:03:26 +0100

    Implementado a página de apresentação de artigo

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,21 @@
+<template>
+    <main class="container">
+        <h1>{{doc.title}}</h1>
+        <img :src="require(`~/assets/images/${doc.img}`)" alt="">
+        <nuxt-content :document="doc"/>
+    </main>
+</template>
+
+<script>
+export default {
+ 
+  async asyncData({$content, params}) {
+    const doc = await $content('blog', params.slug).fetch()
+    return {doc}
+  }
+}
+</script>
+
+<style>
+    .container { max-width: 800px; margin: 50px auto; }
+</style>
