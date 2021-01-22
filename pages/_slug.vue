a4bb3065475f1efc1562c5f0fddee2e598a15330
Jefth Valentim <jefthvalentim@hotmail.com>
2021-01-22 09:20:32 +0100

    adicionado uma página para a listagem dos ficheiros json

---
+++ pages/_slug.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="text" name="" v-model="search" @input="$fetch">
+    <div v-for="file in files">
+      <p>{{file.title}}</p>
+      <p>{{file.description}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+  data() {
+    return {
+      search: '',
+      files: []
+    }
+  },
+  async fetch() {
+    this.files = await this.$content('movies').search(this.search).fetch()
+  }
+}
+</script>
